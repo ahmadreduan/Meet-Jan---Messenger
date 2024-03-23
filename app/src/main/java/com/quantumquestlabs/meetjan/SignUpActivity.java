f5079f63911bf17e4540eb355026fc430c3f7407
reduan-ahmad <reduanahmad363737@gmail.com>
2024-03-23 07:04:34 +0600

    ActivitySignUpBinding binding add

--- app/src/main/java/com/quantumquestlabs/meetjan/SignUpActivity.java
+++ app/src/main/java/com/quantumquestlabs/meetjan/SignUpActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.quantumquestlabs.meetjan.databinding.ActivitySignUpBinding;
+
 public class SignUpActivity extends AppCompatActivity {
+
+    ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
 
         // Hide the action bar
